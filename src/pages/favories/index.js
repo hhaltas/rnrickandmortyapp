e8c5a41ade4f0e92d89a6420e309c0b1196e7b1a
hh <55509425+hhaltas@users.noreply.github.com>
2024-06-10 20:56:55 +0300

    setup file system structs

---
+++ src/pages/favories/index.js
@@ -0,0 +1,14 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+const favoriesIndex = () => {
+  return (
+    <View>
+      <Text>favoriesIndex</Text>
+    </View>
+  );
+};
+
+export default favoriesIndex;
+
+const styles = StyleSheet.create({});
---
+++ src/pages/home/index.js
@@ -0,0 +1,12 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+const homeIndex = () => {
+  return (
+    <View>
+      <Text>homeIndex</Text>
+    </View>
+  );
+};
+
+export default homeIndex;
